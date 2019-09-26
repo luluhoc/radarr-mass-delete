2eab74fca83f0aa500da018a31ddc5f2808ce16b
LULU <lucek111@gmail.com>
2019-09-26 21:08:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const request = require('request');
 const rp = require('request-promise');
 
 // settings
-const radarrAPI = 'YOUR_API_KEY' // API Key From Radarr
+const radarrAPI = 'YOUR_API_KEY'; // API Key From Radarr
 const addExclusion = true; // true if you want to add exclusions // default true
-const deleteFiles = true // true if you want to delete movie from disk // default true
+const deleteFiles = true; // true if you want to delete movie from disk // default true
 const keyOmdb = 'KEY_TO_OMDB'; // https://www.omdbapi.com/
-const desiredRating = 6 // all the movies below and equal to this rating are gonna be deleted
-const radarrUrl = 'https://radarr.YOURDOMAIN.com/api/movie/'
+const desiredRating = 6; // all the movies below and equal to this rating are gonna be deleted
+const radarrUrl = 'https://YOURDOMAIN.com/api/movie/'; // the link should have the exact structure like the one in example
 
 const radarrGet = {
   uri: `${radarrUrl}`,
@@ -20,20 +20,20 @@ const radarrGet = {
 function deleteMovies() {
   rp(radarrGet)
     .then((body) => {
-      const data = JSON.parse(body)
+      const data = JSON.parse(body);
       data.forEach(e => {
         const movie = {
           rId: e.id,
           imdbId: e.imdbId,
           title: e.title,
-        }
+        };
         rp(`http://www.omdbapi.com/?apikey=${keyOmdb}&i=${movie.imdbId}`)
           .then((body) => {
             const data = JSON.parse(body);
             movie.imdbVotes = data.imdbVotes;
             movie.imdbRating = Number(data.imdbRating);
             if (movie.imdbRating <= desiredRating) {
-              console.log(`Deleted ${movie.title}`)
+              console.log(`Deleted ${movie.title}`);
               var options = {
                 method: 'DELETE',
                 uri: `${radarrUrl}${movie.rId}?addExclusion=${addExclusion}&$deleteFiles=${deleteFiles}`,
