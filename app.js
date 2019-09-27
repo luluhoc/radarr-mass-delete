9666bffe6c33a9324a2292349df8c4d0a54cf91a
LULU <lucek111@gmail.com>
2019-09-27 12:59:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function deleteMovies() {
                   'X-Api-Key': radarrAPI,
                 }
               };
-              rp(options).then((item) => console.log(item)).catch(err => console.log(err))
+              rp(options).then((item) => console.log('done')).catch(err => console.log(err))
             }
           })
           .catch(err => console.log(err))
